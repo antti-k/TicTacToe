98edd0fb95985c1d1d397e91240e99cd711750da
Antti Karvanen <karvanen.antti@gmail.com>
2016-12-11 12:34:55 +0200

    Graphical interface finished Turn based gameplay implemented Locking squares not yet implemented Victory determination not yet implemented

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var crossTurn = true
+    
+    
+    
+    @IBAction func buttonPushed(_ sender: AnyObject) {
+        
+        if crossTurn == true {
+            crossTurn = false
+            sender.setImage(UIImage(named: "cross.png"), for: [])
+        } else {
+            crossTurn = true
+            sender.setImage(UIImage(named: "nought.png"), for: [])
+        }
+        
+        
+//        print("button pushed")
+//        
+//        sender.setImage(UIImage(named: "cross.png"), for: [])
+//        
+//        print(sender.tag)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
